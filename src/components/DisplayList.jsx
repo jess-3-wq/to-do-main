cea1fae83d6c4652dc47a3a66b4c013d7628baee
Jesse <jesse.yegon@student.moringaschool.com>
2025-04-25 16:51:24 -0400

    todooo

--- src/components/DisplayList.jsx
+++ src/components/DisplayList.jsx
@@ -1,45 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Delete from './Delete';
 
-function DisplayList({todos,setTodo}) {
-    
-    const [loading,setLoading]=useState(true)
-     const handleDelete=(id)=>{
-              setTodo(todos.filter((todo)=>todo.id !== id))
-     }
+function DisplayList({ todos, setTodo }) {
+  const [loading, setLoading] = useState(true);
 
 
-     function handleChange(id){
-      setTodo((previousTodos)=>previousTodos.map((todo)=>todo.id===id?{...todos, completed: !todo.completed}: todos))
-     }
+  const handleDelete = (id) => {
+    setTodo(todos.filter((todo) => todo.id !== id));
+  };
 
-
-    useEffect(()=>{
-    fetch("https://my-data-kappa.vercel.app/todos")
-    .then(resp=>resp.json())
-    .then(data=>{setTodo(data);
-                setLoading(false)})},[])
-    
-    if(loading) return <p>Loading ......</p>
-
-
-    console.log(todos)
   
-    const list=todos.map((todo)=>(
-      <div className='list' key={todo.id}>
-      
-       <input type="checkbox" className='checkbox'  onChange={()=> handleChange(todo.id)} />
-    
-      <li className={todo.completed ? 'strike':''}>{todo.title}</li>
-    
-     <Delete onDelete={handleDelete}id={todo.id}/>
-     </div>)
-     )
-        return (
-    <ol>
-       {list}
-    </ol>
-  )
+  function handleChange(id) {
+    setTodo((previousTodos) =>
+      previousTodos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
+    );
+  }
+
+  useEffect(() => {
+    fetch('https://my-data-kappa.vercel.app/todos')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTodo(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading ......</p>;
+
+  const list = todos.map((todo) => (
+    <div className="list" key={todo.id}>
+      <input
+        type="checkbox"
+        className="checkbox"
+        checked={todo.completed} // Check box state based on completed
+        onChange={() => handleChange(todo.id)} // Trigger handleChange on checkbox click
+      />
+      <li className={todo.completed ? 'strike' : ''}>{todo.title}</li>
+      <Delete onDelete={handleDelete} id={todo.id} />
+    </div>
+  ));
+
+  return <ol>{list}</ol>;
 }
 
-export default DisplayList
+export default DisplayList;
